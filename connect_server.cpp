6b21700de0f57164a19677befb1f5f27532c0417
benja <benjamin.mosso1701@alumnos.ubiobio.cl>
2023-08-01 17:16:36 -0400

    cambios pendientes, reparar funcion descargade archivos

--- connect_server.cpp
+++ connect_server.cpp
@@ -1,82 +1,185 @@
 #include <iostream>
-#include <libssh/libssh.h>
+#include <string>
+#include <curl/curl.h>
+#include <fstream> // Librería para trabajar con archivos
+#include <unistd.h> // Librería para obtener directorio de descargas en Linux
 
-int main() {
-    // Conexión al servidor SFTP
-    ssh_session sshSession = ssh_new();
-    if (sshSession == nullptr) {
-        std::cerr << "Error al crear la sesión SSH." << std::endl;
-        return 1;
-    }
+using namespace std;
 
-    std::string serverIpAddress = "172.20.0.2";
-    std::string username = "benja";
-    int port = 2222; // Puerto SFTP predeterminado es 22
+// Estructura para almacenar la información de la respuesta del servidor FTP
+struct FtpResponse {
+    std::string url;
+    std::string listing;
+};
 
-    ssh_options_set(sshSession, SSH_OPTIONS_HOST, serverIpAddress.c_str());
-    ssh_options_set(sshSession, SSH_OPTIONS_USER, username.c_str());
-    ssh_options_set(sshSession, SSH_OPTIONS_PORT, &port);
-
-    int rc = ssh_connect(sshSession);
-    if (rc != SSH_OK) {
-        std::cerr << "Error al conectar al servidor: " << ssh_get_error(sshSession) << std::endl;
-        ssh_free(sshSession);
-        return 1;
-    }
-
-    // Autenticación con contraseña (opcional)
-    std::string password = "5426";
-    rc = ssh_userauth_password(sshSession, nullptr, password.c_str());
-    if (rc != SSH_AUTH_SUCCESS) {
-        std::cerr << "Error de autenticación: " << ssh_get_error(sshSession) << std::endl;
-        ssh_disconnect(sshSession);
-        ssh_free(sshSession);
-        return 1;
-    }
-
-    // Abrir una sesión SFTP
-    sftp_session sftpSession = sftp_new(sshSession);
-    if (sftpSession == nullptr) {
-        std::cerr << "Error al crear la sesión SFTP: " << ssh_get_error(sshSession) << std::endl;
-        ssh_disconnect(sshSession);
-        ssh_free(sshSession);
-        return 1;
-    }
-
-    rc = sftp_init(sftpSession);
-    if (rc != SSH_OK) {
-        std::cerr << "Error al inicializar la sesión SFTP: " << ssh_get_error(sshSession) << std::endl;
-        sftp_free(sftpSession);
-        ssh_disconnect(sshSession);
-        ssh_free(sshSession);
-        return 1;
-    }
-
-    // Ruta de la carpeta a listar
-    std::string remoteFolderPath = "/ftp";
-
-    // Obtener la lista de archivos de la carpeta remota
-    sftp_dir dir = sftp_opendir(sftpSession, remoteFolderPath.c_str());
-    if (dir == nullptr) {
-        std::cerr << "Error al abrir la carpeta remota: " << ssh_get_error(sshSession) << std::endl;
-        sftp_free(sftpSession);
-        ssh_disconnect(sshSession);
-        ssh_free(sshSession);
-        return 1;
-    }
-
-    // Recorrer y mostrar los nombres de los archivos en la carpeta remota
-    struct sftp_attributes attributes;
-    while ((attributes = sftp_readdir(sftpSession, dir)) != nullptr) {
-        std::cout << attributes->name << std::endl;
-        sftp_attributes_free(attributes);
-    }
-
-    // Cerrar la carpeta y liberar recursos
-    sftp_closedir(dir);
-    sftp_free(sftpSession);
-    ssh_disconnect(sshSession);
-    ssh_free(sshSession);
-
-    return 0;
+// Función de escritura para recibir los datos del listado de archivos
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, FtpResponse* output) {
+    size_t total_size = size * nmemb;
+    output->listing.append(static_cast<char*>(contents), total_size);
+    return total_size;
 }
+
+// Función para obtener la lista de archivos
+bool GetFtpListing(const std::string& ftp_url, const std::string& username, const std::string& password, std::string& listing) {
+    // Inicializar libcurl
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    // Crear un objeto CURL
+    CURL* curl = curl_easy_init();
+
+    if (curl) {
+        FtpResponse response;
+        response.url = ftp_url;
+
+        // Configurar la URL del servidor FTP
+        curl_easy_setopt(curl, CURLOPT_URL, response.url.c_str());
+
+        // Si requiere autenticación, proporcionar usuario y contraseña
+        if (!username.empty() && !password.empty()) {
+            std::string credentials = username + ":" + password;
+            curl_easy_setopt(curl, CURLOPT_USERPWD, credentials.c_str());
+        }
+
+        // Configurar la función de escritura para recibir los datos del listado de archivos
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        // Realizar la solicitud
+        CURLcode res = curl_easy_perform(curl);
+
+        // Comprobar si la solicitud fue exitosa
+        if (res == CURLE_OK) {
+            // Extraer solo los nombres de los directorios del resultado
+            size_t start_pos = 0;
+            size_t end_pos = 0;
+            int file_count = 0; // Contador de archivos
+            while (true) {
+                end_pos = response.listing.find('\n', start_pos);
+                if (end_pos == std::string::npos)
+                    break;
+                std::string file_line = response.listing.substr(start_pos, end_pos - start_pos);
+                size_t file_name_start = file_line.find_last_of(' ');
+                if (file_name_start != std::string::npos) {
+                    std::string file_name = file_line.substr(file_name_start + 1);
+                    if (!file_name.empty() && file_name != "." && file_name != "..") {
+                        file_count++;
+                        listing += std::to_string(file_count) + ". " + file_name + "\n"; // Mostrar el número del archivo
+                    }
+                }
+                start_pos = end_pos + 1;
+            }
+        } else {
+            // Mostrar el error en caso de que falle la solicitud
+            std::cerr << "Error al obtener el listado de archivos: " << curl_easy_strerror(res) << std::endl;
+        }
+
+        // Limpiar y cerrar la conexión
+        curl_easy_cleanup(curl);
+    } else {
+        std::cerr << "Error al inicializar libcurl." << std::endl;
+    }
+
+    // Finalizar libcurl
+    curl_global_cleanup();
+
+    return true;
+}
+
+// Modificar la firma de la función DownloadFile para que reciba un puntero a ofstream
+bool DownloadFile(const std::string& file_url, std::ofstream* fp) {
+    // Inicializar libcurl
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    // Crear un objeto CURL
+    CURL* curl = curl_easy_init();
+
+    if (curl) {
+        // Configurar la URL del archivo a descargar
+        curl_easy_setopt(curl, CURLOPT_URL, file_url.c_str());
+
+        // Configurar la función de escritura para recibir los datos del archivo
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, [](void* contents, size_t size, size_t nmemb, std::ofstream* fp) {
+            // Escribir los datos recibidos en el ofstream
+            return fp->write(static_cast<char*>(contents), size * nmemb);
+        });
+
+        // Configurar el ofstream como el parámetro para la función de escritura
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+        // Realizar la solicitud
+        CURLcode res = curl_easy_perform(curl);
+
+        // Limpiar y cerrar la conexión
+        curl_easy_cleanup(curl);
+
+        // Comprobar si la descarga fue exitosa
+        if (res == CURLE_OK) {
+            return true;
+        } else {
+            std::cerr << "Error al descargar el archivo: " << curl_easy_strerror(res) << std::endl;
+        }
+    } else {
+        std::cerr << "Error al inicializar libcurl." << std::endl;
+    }
+
+    // Finalizar libcurl
+    curl_global_cleanup();
+
+    return false;
+}
+
+void descargarArchivo(std::string ftp_url, int opcion) {
+    std::string username = "benja";
+    std::string password = "5426";
+
+    std::string listing;
+    if (GetFtpListing(ftp_url, username, password, listing)) {
+        std::cout << "Listado de archivos:\n" << listing << std::endl;
+
+        std::string file_to_download;
+        size_t start_pos = listing.find(std::to_string(opcion) + ".");
+        if (start_pos != std::string::npos) {
+            start_pos = listing.find(' ', start_pos);
+            size_t end_pos = listing.find('\n', start_pos);
+            if (end_pos != std::string::npos) {
+                file_to_download = listing.substr(start_pos + 1, end_pos - start_pos - 1);
+                std::cout << "Descargando archivo: " << file_to_download << std::endl;
+
+                std::string download_dir = getenv("HOME");
+                std::ofstream outfile(download_dir + "/descargas/" + file_to_download, std::ofstream::binary); // Abrir el archivo en modo binario
+                if (outfile.is_open()) {
+                    std::string file_url = ftp_url + file_to_download;
+                    if (DownloadFile(file_url, &outfile)) {
+                        std::cout << "Archivo descargado exitosamente." << std::endl;
+                    } else {
+                        std::cerr << "Error al descargar el archivo." << std::endl;
+                    }
+                    outfile.close(); // Cerrar el archivo después de la descarga
+                } else {
+                    std::cerr << "Error al abrir el archivo para escribir." << std::endl;
+                }
+            } else {
+                std::cerr << "Opción inválida." << std::endl;
+            }
+        } else {
+            std::cerr << "Opción inválida." << std::endl;
+        }
+    }
+}
+
+void listar(std::string ftp_url) {
+    std::string username = "benja";
+    std::string password = "5426";
+
+    std::string listing;
+    if (GetFtpListing(ftp_url, username, password, listing)) {
+        std::cout << "Listado de archivos:\n" << listing << std::endl;
+
+        int opcion;
+        std::cout << "Ingrese el número del archivo que desea descargar: ";
+        std::cin >> opcion;
+
+        descargarArchivo(ftp_url, opcion);
+    }
+}
+
--- ver_asignaturas.cpp
+++ ver_asignaturas.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream> // Librería para trabajar con archivos
 
 using namespace std;
+extern void listar(std::string ftp_url);
+extern bool DownloadFile(const std::string& url, std::ofstream* outfile); // Incluir la declaración de DownloadFile
 
 vector<vector<string>> semestre = {
         {"Algebra 1", "Nociones de Computacion e Informatica", "Algoritmos y Bases de la Programacion", "Introduccion a la Ingenieria", "Comunicacion y Argumentacion"},
@@ -15,18 +18,6 @@ vector<vector<string>> semestre = {
         {"Electivos de Especialidad", "Proyecto Final de Carrera"}
 };
 
-vector<vector<string>> asig_docs = {
-        {"documento 1", "documento 2", "documento 3"}
-};
-
-vector<vector<string>> asig_libs = {
-        {"libro 1", "libro 2", "libro 3"}
-};
-
-vector<vector<string>> asig_extras = {
-        {"extra 1", "extra 2", "extra 3"}
-};
-
 void verAsignatura(int semestre2) {
     if (semestre2 < 1 || semestre2 > 8) {
         cout << "Semestre invalido" << endl;
@@ -35,36 +26,65 @@ void verAsignatura(int semestre2) {
         for (int i = 0; i < semestre[semestre2 - 1].size(); i++) {
             cout << i + 1 << ". " << semestre[semestre2 - 1][i] << endl;
         }
+
+        std::string sem = "semestre";
+        std::string semftp = sem + std::to_string(semestre2);
+
         int asignatura;
         cout << "Ingrese el numero de la asignatura que desea visualizar (1-" << semestre[semestre2 - 1].size() << "): ";
         cin >> asignatura;
+
+        std::string asig = "asignatura";
+        std::string asigftp = asig + std::to_string(asignatura);
+        std::string ftpurl = "ftp://192.168.1.117:21/" +semftp + "/" +asigftp +"/";
+
         cout << endl;
         cout << "----------------------------------------" << endl;
         cout << "Biblioteca de la asignatura " << asignatura << ":" << endl;
         cout << endl;
-        cout << "Documentos: " << endl;
-        int contador = 1;
-        for (int i = 0; i < asig_docs[0].size(); i++) {
-            cout << "   " << contador << ". " << asig_docs[0][i] << endl;
-            contador++;
-        }
-        cout << "Libros: " << endl;
-        for (int i = 0; i < asig_libs[0].size(); i++) {
-            cout << "   " << contador << ". " << asig_libs[0][i] << endl;
-            contador++;
-        }
-        cout << "Extras:" << endl;
-        for (int i = 0; i < asig_extras[0].size(); i++) {
-            cout << "   " << contador << ". " << asig_extras[0][i] << endl;
-            contador++;
-        }
+
+        listar(ftpurl);
+
         cout << endl;
         cout << "----------------------------------------" << endl;
         int opcion;
-        cout << "Ingrese el numero del archivo/link que desea revisar: ";
+        cout << "Ingrese el numero del archivo/link que desea descargar: ";
         cin >> opcion;
 
-        // Aquí puedes implementar la lógica para preguntar al usuario si le funcionó o no,
-        // y ofrecer posibles soluciones en caso de problemas.
+        // Verificar si el número de archivo seleccionado es válido
+        if (opcion >= 1 && opcion <= semestre[semestre2 - 1].size()) {
+            // Obtener el nombre del archivo seleccionado
+            std::string file_to_download = semestre[semestre2 - 1][opcion - 1];
+
+            // Modificar el nombre del archivo para que sea compatible con el sistema de archivos de Linux
+            for (char& c : file_to_download) {
+                if (c == ' ' || c == '/') {
+                    c = '_';
+                }
+            }
+
+            // Obtener el directorio "Descargas" en Linux
+            std::string home_dir = getenv("HOME");
+            std::string downloads_dir = home_dir + "/Descargas/";
+
+            // Construir la URL completa del archivo a descargar
+            std::string file_url = ftpurl + file_to_download;
+
+            // Crear un objeto ofstream para escribir el archivo local
+            std::ofstream outfile(downloads_dir + file_to_download, std::ofstream::binary);
+
+            if (outfile) {
+                // Descargar el archivo
+                if (DownloadFile(file_url, &outfile)) {
+                    cout << "Descarga completada. El archivo se encuentra en: " << downloads_dir << file_to_download << endl;
+                } else {
+                    cout << "No se pudo descargar el archivo." << endl;
+                }
+            } else {
+                cout << "No se pudo crear el archivo local." << endl;
+            }
+        } else {
+            cout << "Opción inválida. El número de archivo seleccionado no es válido." << endl;
+        }
     }
 }
